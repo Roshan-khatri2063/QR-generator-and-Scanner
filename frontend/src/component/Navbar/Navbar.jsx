b20679d3e64ad5368d1fa2282421ee02062aa99b
Roshan Khatri <roshankhatri2063@gmail.com>
2026-05-09 08:59:31 +0545

    Implement reusable frontend navbar component

---
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+      <nav className="navbar">
+          <h2 className="logo"> QRHub </h2>
+
+          <ul className="nav-links">
+
+              <li>
+                  <link to="/"/>
+                  Home <link/>
+              </li>
+
+              <li>
+                  <Link to="/generate">Generate</Link>
+              </li>
+
+              <li>
+                  <Link to="/scan">Scan</Link>
+              </li>
+
+          </ul>
+      </nav>
+    );
+}
+export default Navbar;

92e8754b9161ee1913bd48889d4120fb6d5c6316
Roshan Khatri <roshankhatri2063@gmail.com>
2026-05-09 08:45:55 +0545

    create backend API status endpoint

---
+++ backend/src/main/java/com/qrcode/backend/controller/StatusServlet.java
@@ -0,0 +1,22 @@
+package com.qrcode.backend.controller;
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/status")
+public class StatusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+
+        response.setContentType("application/json");
+
+        response.getWriter().write(
+                "{\"message\":\"Backend is running successsfully\"}"
+        );
+    }
+}
--- backend/src/main/java/com/qrcode/backend/controller/controller.java
+++
@@ -1,4 +0,0 @@
-package com.qrcode.backend.controller;
-
-public class controller {
-}

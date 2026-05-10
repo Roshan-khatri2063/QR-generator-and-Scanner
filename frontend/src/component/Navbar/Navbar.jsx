5ed8e9a5e1652c64701fb0ecfe329bfcde5e2684
Roshan Khatri <roshankhatri2063@gmail.com>
2026-05-10 13:06:38 +0545

    update Navbar

--- frontend/src/component/Navbar/Navbar.jsx
+++ frontend/src/component/Navbar/Navbar.jsx
@@ -3,25 +3,18 @@ import "./Navbar.css";
 
 function Navbar() {
     return (
-      <nav className="navbar">
-          <h2 className="logo"> QRHub </h2>
+        <nav className="navbar">
+            <h2 className="logo">QRHub</h2>
 
-          <ul className="nav-links">
-
-              <li>
-                  <Link to="/">Home </Link>
-              </li>
-
-              <li>
-                  <Link to="/generate">Generate</Link>
-              </li>
-
-              <li>
-                  <Link to="/scan">Scan</Link>
-              </li>
-
-          </ul>
-      </nav>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/generate">Generate</Link></li>
+                <li><Link to="/scan">Scan</Link></li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+            </ul>
+        </nav>
     );
 }
+
 export default Navbar;
